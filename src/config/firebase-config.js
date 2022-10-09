743c0471a8fff3dbf8e865b3e88c1e24320c62a3
otakuchan <ianbjs@gmail.com>
2022-10-08 21:34:31 -0700

    CAP-8 build: install firebase and add metro.config.js to parse .cjs extension

---
+++ src/config/firebase-config.js
@@ -0,0 +1,16 @@
+// import { initializeApp } from "firebase/app";
+// import { getAnalytics } from "firebase/analytics";
+
+export const firebaseConfig = {
+	apiKey: "AIzaSyD_8v1cLnLsLUAX4_VFY_OADLsgqU78BHQ",
+	authDomain: "tradeapp-44be9.firebaseapp.com",
+	projectId: "tradeapp-44be9",
+	storageBucket: "tradeapp-44be9.appspot.com",
+	messagingSenderId: "790607552934",
+	appId: "1:790607552934:web:7a20176669252e69d16f67",
+	measurementId: "G-XJCB22EGJP",
+};
+
+// Initialize Firebase
+// export const app = initializeApp(firebaseConfig);
+// export const analytics = getAnalytics(app); 
--- src/screens/Login.jsx
+++ src/screens/Login.jsx
@@ -1,131 +1,151 @@
 import React, { useEffect, useState } from "react";
 import {
-    Box,
-    Text,
-    Heading,
-    VStack,
-    FormControl,
-    Input,
-    Link,
-    Button,
-    HStack,
-    Center,
+	Box,
+	Text,
+	Heading,
+	VStack,
+	FormControl,
+	Input,
+	Link,
+	Button,
+	HStack,
+	Center,
 } from "native-base";
-import { useDispatch, useSelector } from 'react-redux'
-import { login } from '../redux/action'
+import { useDispatch, useSelector } from "react-redux";
+import { login } from "../redux/action";
+
+// Firebase
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
+import { initializeApp } from "firebase/app";
+import { firebaseConfig } from "../config/firebase-config";
+
 
 const Login = () => {
+	const { error } = useSelector((state) => state.auth);
 
-    const { error } = useSelector(state=>state.auth)
+	const dispatch = useDispatch();
 
-    const dispatch = useDispatch()
+	const [email, setEmail] = useState("");
+	const [password, setPassword] = useState("");
 
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
+	// const loginHandler = () => {
+	// 	dispatch(login(email, password));
+	// };
 
-    const loginHandler = () => {
-        dispatch(login(email, password))
-    };
+	// useEffect(() => {
+	// 	if (error) {
+	// 		alert(error);
+	// 		dispatch({ type: "clearError" });
+	// 	}
+	// }, [error, dispatch, alert]);
 
-    useEffect(() => {
-        if(error){
-            alert(error)
-            dispatch({ type: "clearError" })
-        }
+	// console.log(error);
 
-    }, [error, dispatch, alert])
+	// Firebase
+	const auth = getAuth();
 
-    console.log(error)
+	const loginHandler = () => {
+		signInWithEmailAndPassword(auth, email, password)
+			.then((userCredential) => {
+				// Signed in
+				const user = userCredential.user;
+				// ...
+			})
+			.catch((error) => {
+				const errorCode = error.code;
+				const errorMessage = error.message;
+			});
+	};
 
-    return (
-        <Center w="100%">
-            <Box safeArea p="2" py="8" w="90%" maxW="290">
-                <Heading
-                    size="lg"
-                    fontWeight="600"
-                    color="coolGray.800"
-                    _dark={{
-                        color: "warmGray.50",
-                    }}
-                >
-                    Welcome
-                </Heading>
-                <Heading
-                    mt="1"
-                    _dark={{
-                        color: "warmGray.200",
-                    }}
-                    color="coolGray.600"
-                    fontWeight="medium"
-                    size="xs"
-                >
-                    Sign in to continue!
-                </Heading>
+	return (
+		<Center w="100%">
+			<Box safeArea p="2" py="8" w="90%" maxW="290">
+				<Heading
+					size="lg"
+					fontWeight="600"
+					color="coolGray.800"
+					_dark={{
+						color: "warmGray.50",
+					}}
+				>
+					Welcome
+				</Heading>
+				<Heading
+					mt="1"
+					_dark={{
+						color: "warmGray.200",
+					}}
+					color="coolGray.600"
+					fontWeight="medium"
+					size="xs"
+				>
+					Sign in to continue!
+				</Heading>
 
-                <VStack space={3} mt="5">
-                    <FormControl>
-                        <FormControl.Label>Email</FormControl.Label>
-                        <Input
-                            placeholder="example@gmail.com"
-                            value={email}
-                            onChangeText={setEmail}
-                            autoCapitalize='none'
-                        />
-                    </FormControl>
-                    <FormControl>
-                        <FormControl.Label>Password</FormControl.Label>
-                        <Input
-                            secureTextEntry
-                            type="password"
-                            placeholder="Enter your password"
-                            value={password}
-                            onChangeText={setPassword}
-                        />
-                        <Link
-                            _text={{
-                                fontSize: "xs",
-                                fontWeight: "500",
-                                color: "indigo.500",
-                            }}
-                            alignSelf="flex-end"
-                            mt="1"
-                        >
-                            Forget Password?
-                        </Link>
-                    </FormControl>
-                    <Button
-                        mt="2"
-                        colorScheme="indigo"
-                        disabled={!email || !password}
-                        onPress={loginHandler}
-                    >
-                        Sign in
-                    </Button>
-                    <HStack mt="6" justifyContent="center">
-                        <Text
-                            fontSize="sm"
-                            color="coolGray.600"
-                            _dark={{
-                                color: "warmGray.200",
-                            }}
-                        >
-                            I'm a new user.{" "}
-                        </Text>
-                        <Link
-                            _text={{
-                                color: "indigo.500",
-                                fontWeight: "medium",
-                                fontSize: "sm",
-                            }}
-                            href="#"
-                        >
-                            Sign Up
-                        </Link>
-                    </HStack>
-                </VStack>
-            </Box>
-        </Center>
-    );
+				<VStack space={3} mt="5">
+					<FormControl>
+						<FormControl.Label>Email</FormControl.Label>
+						<Input
+							placeholder="example@gmail.com"
+							value={email}
+							onChangeText={setEmail}
+							autoCapitalize="none"
+						/>
+					</FormControl>
+					<FormControl>
+						<FormControl.Label>Password</FormControl.Label>
+						<Input
+							secureTextEntry
+							type="password"
+							placeholder="Enter your password"
+							value={password}
+							onChangeText={setPassword}
+						/>
+						<Link
+							_text={{
+								fontSize: "xs",
+								fontWeight: "500",
+								color: "indigo.500",
+							}}
+							alignSelf="flex-end"
+							mt="1"
+						>
+							Forget Password?
+						</Link>
+					</FormControl>
+					<Button
+						mt="2"
+						colorScheme="indigo"
+						disabled={!email || !password}
+						onPress={loginHandler}
+					>
+						Sign in
+					</Button>
+					<HStack mt="6" justifyContent="center">
+						<Text
+							fontSize="sm"
+							color="coolGray.600"
+							_dark={{
+								color: "warmGray.200",
+							}}
+						>
+							I'm a new user.{" "}
+						</Text>
+						<Link
+							_text={{
+								color: "indigo.500",
+								fontWeight: "medium",
+								fontSize: "sm",
+							}}
+							href="#"
+						>
+							Sign Up
+						</Link>
+					</HStack>
+				</VStack>
+			</Box>
+		</Center>
+	);
 };
 
 export default Login;
