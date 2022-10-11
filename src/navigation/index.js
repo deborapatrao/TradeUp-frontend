bb4ed0d98c4da208bdd06886869f4e8cd36be2b5
otakuchan <ianbjs@gmail.com>
2022-10-11 05:03:48 -0700

    CAP-8 build: made changes on file structure

---
+++ src/navigation/index.js
@@ -0,0 +1,15 @@
+import { useAuthentication } from "../hook/useAuthentication";
+import AppStack from "./AppStack";
+import AuthStack from "./AuthStack";
+import { NativeBaseProvider } from "native-base";
+import { StatusBar } from "expo-status-bar";
+
+export default function RootNavigation() {
+	const { email } = useAuthentication();
+	return (
+		<NativeBaseProvider>
+			<StatusBar style="auto" />
+			{email ? <AppStack /> : <AuthStack />}
+		</NativeBaseProvider>
+	);
+}
