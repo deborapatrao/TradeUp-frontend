1437cacec1ceef9e644c02dad15642d98593ea93
alexey432 <alexorl432@gmail.com>
2022-10-26 14:57:39 -0700

    CAP-30 serverUrl change

--- src/redux/action.js
+++ src/redux/action.js
@@ -4,8 +4,8 @@ import {
     getAuth,
     signInWithCustomToken,
 } from 'firebase/auth';
-// const serverUrl = "http://localhost:8080/api";
-const serverUrl = "http://192.168.194.246:8080/api";
+const serverUrl = "http://localhost:8080/api";
+// const serverUrl = "http://192.168.194.246:8080/api";
 
 export const login = (email, password) => async (dispatch) => {
 
