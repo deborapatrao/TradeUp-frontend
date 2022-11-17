1da70d5cafecc4e3e23477aaba24e73d4a6976ed
Siuly <siulygt@gmail.com>
2022-11-16 16:47:47 -0800

    Final changes to the leader board with the mock data

--- src/screens/leaderboard/Leaderboard.jsx
+++ src/screens/leaderboard/Leaderboard.jsx
@@ -8,8 +8,9 @@ import {
     View,
 	Image,
 	VStack,
+	Box,
 } from "native-base";
-import Users from '../../assets/images/icons/alert-icon.png';
+import Profile from '../../assets/images/icons/profile.png';
 import { StyleSheet, ScrollView} from "react-native";
 
 const Leaderboard = () => {
@@ -27,7 +28,9 @@ const Leaderboard = () => {
 		name: "Alfred",
 		percentage: "0.25%",
 		rank: 3,
-		},
+		}			
+	]
+	const datapt2 = [
 		{
 		name: "Gordon",
 		percentage: "5%",
@@ -45,46 +48,63 @@ const Leaderboard = () => {
 		},
 		{
 		name: "Harley",
-		percentage: "0.25%",
+		percentage: "0.35%",
 		rank: 7,
+		},
+		{
+		name: "Ivy",
+		percentage: "0.75%",
+		rank: 8,
+		},
+		{
+		name: "Harvey",
+		percentage: "0.15%",
+		rank: 9,
+		},
+		{
+		name: "Jonathan",
+		percentage: "0.85%",
+		rank: 10,
 		}		
 	]
 
 
 	return (
 		<>
-		<HStack justifyContent={'space-between'} ml={3} mr={3} mb={3} >
-			<VStack backgroundColor={'black'}  flexGrow={1} alignItems={'center'}  h={'40'} justifyContent={'flex-end'} pb={'3'}>
-				<Image source={Users} alt={'user-image'}  borderRadius={'50'} />
-				<Text>{data[0].name}</Text>
-				<Text>{data[0].percentage}</Text>
-			</VStack>
-			<VStack backgroundColor={'black'} flexGrow={2} alignItems={'center'} h={'40'} justifyContent={'center'}>
-				<Image source={Users} alt={'user-image'} borderRadius={'50'} backgroundColor={'white'}/>
-				<Text>{data[1].name}</Text>
-				<Text>{data[1].percentage}</Text>
-			</VStack>
-			<VStack backgroundColor={'black'} flexGrow={1} alignItems={'center'} h={'40'} justifyContent={'flex-end'} pb={'3'}>
-			<Image source={Users} alt={'user-image'}  borderRadius={'50'} w={'12'} h={'12'} backgroundColor={'white'}/>	
-				<Text>{data[2].name}</Text>
-				<Text>{data[2].percentage}</Text>
-			</VStack>
-		</HStack>	
+		{/* <Box style={[styles.trapezoid]}> */}
+			<HStack style={styles.leaders}>
+				<VStack style={styles.laterals}>
+					<Image source={Profile} alt={'user-image'} style={styles.imageLaterals}  />
+					<Text>{data[0].name}</Text>
+					<Text>{data[0].percentage}</Text>
+				</VStack>
+				<VStack  style={styles.central}>
+					<Image source={Profile} alt={'user-image'} style={styles.imageCentral} />
+					<Text>{data[1].name}</Text>
+					<Text>{data[1].percentage}</Text>
+				</VStack>
+				<VStack style={styles.laterals}>
+				<Image source={Profile} alt={'user-image'}  style={styles.imageLaterals} />	
+					<Text>{data[2].name}</Text>
+					<Text>{data[2].percentage}</Text>
+				</VStack>
+			</HStack>
+		{/* </Box> */}
 		<ScrollView>
 			<View ml={3} mr={3}>        
         		<HStack style={styles.column}>
         		  <Text>Your Rank: 95th</Text>
         		</HStack>
 
-				{data.map((item) => (
+				{datapt2.map((item) => (
 					<HStack style={[styles.column, styles.tableLine]} alignItems={'center'}>
 					<HStack space={4} alignItems={'center'}>
 					  <Text style={styles.text}>{item.rank} </Text>
-					  <Image source={Users} alt={'user-image'} />
+					  <Image source={Profile} alt={'user-image'} style={styles.imageList} />
 					  <Text style={styles.text}>{item.name} </Text>
 					</HStack>
 					<HStack justifyContent={'flex-end'} w={'60%'}>
-					<Text style={styles.text}>{item.percentage}</Text>
+					<Text style={styles.percentage}>{item.percentage}</Text>
 					</HStack>
 				  </HStack>
 					))}
@@ -103,18 +123,19 @@ const styles = StyleSheet.create({
 	column: {
 	  justifyContent: 'space-between',
 	  marginBottom: 5,
-	  marginTop: 5,
+	  marginTop: 3,
 	  textAlign: 'center',
 	},
   
 	tableLine: {
 	  backgroundColor: 'rgba(204, 204, 204, .1)',
-	  padding: 15,
+	  padding: 10,
 	  borderRadius: 5
 	},
   
 	text:{
-	  color: '#fff'
+	  color: '#fff',
+	  paddingLeft: 8,
 	},
   
 	button: {
@@ -125,7 +146,7 @@ const styles = StyleSheet.create({
 	  borderRadius: 3,
 	  overflow: "hidden",
 	  paddingLeft: 4,
-	  paddingRight: 4
+	  marginRight: 4,
 	},
   
 	percentagePositive: {
@@ -135,7 +156,54 @@ const styles = StyleSheet.create({
 	percentageNegative: {
 	  backgroundColor: '#ff6666',
 	},
-
+	laterals:{
+		flexGrow: 1, 
+		alignItems: 'center', 
+		h: 40, 
+		justifyContent: 'flex-end', 
+		pb: 3
+	}, 
+	central:{
+		flexGrow: 2, 
+		alignItems: 'center', 
+		h: 40, 
+		justifyContent: 'center', 
+		paddingBottom: 15
+	},
+	trapezoid: {
+		width: "90%",
+		height: 0,
+		borderTopWidth: 40,
+		borderTopColor: 'rgba(128, 128, 128, 0.4)',
+		borderLeftWidth: 50,
+		borderLeftColor: 'transparent',
+		borderRightWidth: 50,
+		borderRightColor: 'transparent',
+		borderStyle: 'solid',
+		backgroundColor: 'red',
+	   }, 
+	   imageList:{
+		height: 35,
+		width: 35,
+		borderRadius: 50,
+	},
+	imageLaterals:{
+		height: 80,
+		width: 80,
+		borderRadius: 50,
+	},
+	imageCentral:{
+		height: 120,
+		width: 120,
+		borderRadius: 50,
+	},
+	leaders:{
+		justifyContent: 'space-between', 
+		marginLeft:3, 
+		marginRigh: 3, 
+		maargBottom: 3, 
+		marginTop: 4,
+	}
   
   });
 
