9e984afb8b6211a9b762bf4462f88dc1f0bf7314
alexey432 <alexorl432@gmail.com>
2022-12-06 00:58:26 -0800

    CAP-94 check

--- src/components/containers/market/chart/Label.js
+++ src/components/containers/market/chart/Label.js
@@ -31,22 +31,25 @@ const formatInt = (value) => {
   return cond(lessThan(t, 1), concat(t), concat(t, ",", modulo(value, 1000)));
 };
 
-const format = (value) => {
-  if (Platform.OS === "android") {
-    return concat("$ ", divide(floor(multiply(value, 100)), 100));
-  }
-  const int = floor(value);
-  const dec = floor(multiply(sub(value, int), 100));
-  const formattedDec = cond(
-    eq(dec, 0),
-    "00",
-    cond(lessThan(dec, 10), concat("0", dec), concat(dec))
-  );
-  return concat("$", formatInt(int), ".", formattedDec);
-};
+
 
 
 const Label = ({ domain, size, y, opacity }) => {
+
+  const format = (value) => {
+    if (Platform.OS === "android") {
+      return concat("$ ", divide(floor(multiply(value, 100)), 100));
+    }
+    const int = floor(value);
+    const numLimit = parseFloat(min) > 1 ? 100 : 10000;
+    const dec = floor(multiply(sub(value, int), numLimit));
+    const formattedDec = cond(
+      eq(dec, 0),
+      "00",
+      cond(lessThan(dec, 10), concat("0", dec), concat(dec))
+    );
+    return concat("$", int, ".", formattedDec);
+  };
 
   const min = domain[0]
   const max = domain[1]
@@ -57,7 +60,7 @@ const Label = ({ domain, size, y, opacity }) => {
   });
   return (
     <Animated.View
-      style={[styles.container, { transform: [{ translateY: y }, { translateX: 2 }], opacity }]}
+      style={[styles.container, { transform: [{ translateY: y }, { translateX: 0 }], opacity }]}
     >
       <ReText
         text={format(value)}
--- src/screens/wallet/AssetsScreen.jsx
+++ src/screens/wallet/AssetsScreen.jsx
@@ -1,13 +1,13 @@
 import React, { useState, useEffect } from 'react'
 import {
-    Box,
-    Text,
-    FlatList,
-    HStack,
-    Button,
-    Divider,
+  Box,
+  Text,
+  FlatList,
+  HStack,
+  Button,
+  Divider,
 } from "native-base";
-import {StyleSheet} from 'react-native';
+import { StyleSheet } from 'react-native';
 import { getWalletData } from '../../utils/requests';
 import AssetItem from '../../components/listItems/AssetItem';
 
@@ -16,121 +16,121 @@ import { useSelector } from "react-redux";
 
 
 const AssetsScreen = ({ navigation }) => {
-    const [assetData, setAssetData] = useState(null);
-    const { user } = useSelector((state) => state.auth);
+  const [assetData, setAssetData] = useState(null);
+  const { user } = useSelector((state) => state.auth);
 
-    useEffect(() => {
-        console.log(user);
-        // console.log('check2');
-        // if (user) {
-        // } else {
-        //     console.log('not authenticated 2');
-        // }
-        const unsubscribe = navigation.addListener('focus', async () => {
-            console.log('AssetsScreen focused');
-            const data = await getWalletData('/wallet/assets');
-            console.log('Data: ', data);
-            setAssetData(data.assets);
-        });
-        loadCoins();
-        return unsubscribe;
+  useEffect(() => {
+    console.log(user);
+    // console.log('check2');
+    // if (user) {
+    // } else {
+    //     console.log('not authenticated 2');
+    // }
+    const unsubscribe = navigation.addListener('focus', async () => {
+      console.log('AssetsScreen focused');
+      const data = await getWalletData('/wallet/assets');
+      console.log('Data: ', data);
+      setAssetData(data.assets);
+    });
+    loadCoins();
+    return unsubscribe;
 
-    }, [navigation]);
+  }, [navigation]);
 
-    async function loadCoins(){
-        try {
-          const reponse = await axios.get(`https://api.binance.com/api/v3/ticker/24hr?${symbolsString}`);
-          
+  async function loadCoins() {
+    try {
+      const reponse = await axios.get(`https://api.binance.com/api/v3/ticker/24hr?${symbolsString}`);
 
-    let sortedData = [];
 
-    switch(type){
-      case "24":
-        sortedData =  reponse.data.sort((a, b) => {
-          return toggle
-          ?a.priceChangePercent - b.priceChangePercent
-          :b.priceChangePercent - a.priceChangePercent  
-        });
-        
-        break;
-      case "alphabetical":
-        sortedData = data.sort((a, b) => {
-          return toggle
-          ?a.symbol.localeCompare(b.symbol)
-          :b.symbol.localeCompare(a.symbol)
-        });
-        break;
-      default:
-        sortedData = reponse.data
-      } 
+      let sortedData = [];
+
+      switch (type) {
+        case "24":
+          sortedData = reponse.data.sort((a, b) => {
+            return toggle
+              ? a.priceChangePercent - b.priceChangePercent
+              : b.priceChangePercent - a.priceChangePercent
+          });
+
+          break;
+        case "alphabetical":
+          sortedData = data.sort((a, b) => {
+            return toggle
+              ? a.symbol.localeCompare(b.symbol)
+              : b.symbol.localeCompare(a.symbol)
+          });
+          break;
+        default:
+          sortedData = reponse.data
+      }
 
       setData([...sortedData])
 
-} catch (error) {
-  console.log(error)
-}
-}
+    } catch (error) {
+      console.log(error)
+    }
+  }
 
 
-const handleTypeChange = (selectedType) => {
-setType(selectedType);
-setToggle(!toggle)
-}
+  const handleTypeChange = (selectedType) => {
+    setType(selectedType);
+    setToggle(!toggle)
+  }
 
-    return (
-        <Box bgColor={'primary.bg'} flex={1} px={4}>
+  return (
+    <Box bgColor={'primary.bg'} flex={1} px={4}>
 
-<HStack style={styles.column}>
-          <Button style={styles.background} 
+      <HStack style={styles.column}>
+        <Button style={styles.background}
           onPress={() => handleTypeChange("alphabetical")}
-          >
-            <Text style={styles.text}>Pair</Text>
-            <Text style={styles.text}>USDT</Text>
-          </Button>
+        >
+          <Text style={styles.text}>Pair</Text>
+          <Text style={styles.text}>USDT</Text>
+        </Button>
 
-          <Button style={styles.background} >
-            <Text style={styles.text}>Last</Text>
-            <Text style={styles.text}>price</Text>
-          </Button>
+        <Button style={styles.background} >
+          <Text style={styles.text}>Last</Text>
+          <Text style={styles.text}>price</Text>
+        </Button>
 
-          <Button style={styles.background} 
+        <Button style={styles.background}
           onPress={() => handleTypeChange('24')}
-          >
-            <Text style={{...styles.text, width: 60, textAlign: 'center'}}>24H Change</Text>
-            {/* <Text style={styles.text}>Change</Text> */}
-          </Button>
-        </HStack>
-        
-        <Divider />
+        >
+          <Text style={{ ...styles.text, width: 60, textAlign: 'center' }}>24H Change</Text>
+          {/* <Text style={styles.text}>Change</Text> */}
+        </Button>
+      </HStack>
 
-            <FlatList
-                data={assetData}
-                style={{ paddingHorizontal: 5 }}
-                renderItem={({ item }) => {
-                    return <AssetItem asset={item} />
-                }}
-            />
-        </Box>
-    );
+      <Divider />
+
+      <FlatList
+        data={assetData}
+        style={{ paddingTop: 10 }}
+        renderItem={({ item }) => {
+          return <AssetItem asset={item} />
+        }}
+      />
+    </Box>
+  );
 };
 
 const styles = StyleSheet.create({
-    background: {
-      backgroundColor: '#171122',
-    }, 
-  
-    column: {
-      justifyContent: 'space-between',
-      marginBottom: 5,
-      marginTop: 5,
-      textAlign: 'center',
-    },
-  
-    text:{
-      color: '#fff'
-    },
-  
-  
-  });
+  background: {
+    backgroundColor: '#171122',
+  },
+
+  column: {
+    justifyContent: 'space-between',
+    marginBottom: 5,
+    marginTop: 5,
+    textAlign: 'center',
+  },
+
+  text: {
+    color: '#fff'
+  },
+
+
+});
 
 export default AssetsScreen;
