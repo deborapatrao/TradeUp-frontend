531440ac91c25576a1b8aeeeacb06b4d0a0d736e
alexey432 <alexorl432@gmail.com>
2022-12-07 02:15:58 -0800

    loaders added for wallet screens

--- src/screens/wallet/TradesScreen.jsx
+++ src/screens/wallet/TradesScreen.jsx
@@ -3,14 +3,18 @@ import { Box, Text, FlatList, Center, View } from "native-base";
 import { StyleSheet } from "react-native";
 import { getTradeHistoryData } from "../../utils/requests";
 import HistoryItem from "../../components/listItems/HistoryItem";
+import Loader from "../../components/utils/Loader";
 
 const TradesScreen = ({ navigation }) => {
-	const [tradeHistoryData, setTradeHistoryData] = useState([]);
+	const [tradeHistoryData, setTradeHistoryData] = useState(null);
+	const [loading, setLoading] = useState(false);
 
 	useEffect(() => {
 		const checkedFocus = navigation.addListener('focus', async () => {
+			setLoading(true);
 			await loadTradeHistory();
 			console.log('TradesScreen focused');
+			setLoading(false);
 		});
 
 		return checkedFocus;
@@ -27,16 +31,18 @@ const TradesScreen = ({ navigation }) => {
 
 	return (
 		<View style={{ paddingHorizontal: 20, paddingTop: 20 }}>
-			{tradeHistoryData.length > 0 ? (
-				<FlatList
-					data={tradeHistoryData}
-					renderItem={({ item }) => {
-						return <HistoryItem history={item} />;
-					}}
-				/>
-			) : (
-				<Center style={styles.container}><Text>No data available</Text></Center>
-			)}
+			{loading ? <Box style={{ marginTop: 20 }}><Loader /></Box> :
+				tradeHistoryData !== null ? (
+					<FlatList
+						data={tradeHistoryData}
+						renderItem={({ item }) => {
+							return <HistoryItem history={item} />
+						}}
+					/>
+				) : (
+					<Center style={styles.container}><Text>No data available</Text></Center>
+				)
+			}
 		</View>
 	);
 };
