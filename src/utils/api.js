befab9e7ab84cd121c94f0e4a1a4607d07a17881
Debora Patrao <45935075+deborapatrao@users.noreply.github.com>
2023-04-12 20:24:50 -0700

    Update api.js

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,4 +1,5 @@
 // export const BASE_URL = 'http://192.168.194.246:8080/api';
-export const BASE_URL = 'http://localhost:8080/api';
+//export const BASE_URL = 'http://localhost:8080/api';
+export const BASE_URL = 'https://deboracorreia.com/api';
 // export const BASE_URL = 'https://tangy-melons-repair-24-114-23-210.loca.lt/api';
 // export const BASE_URL = 'https://tradeup-app.ca/api';
